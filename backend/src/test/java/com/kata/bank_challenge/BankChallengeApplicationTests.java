b15f3d4ef885f8eacc8d619412ae6ac90246365c
josteda99 <josteda99@gmail.com>
2026-02-07 10:49:45 -0500

    first commit

---
+++ backend/src/test/java/com/kata/bank_challenge/BankChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.bank_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
